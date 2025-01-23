eaa866c48c0335a25f3d076b570dc5e968dba395
eduardoamorim-dev <amorimm.dev@gmail.com>
2025-01-23 03:26:36 -0300

    note: create database

---
+++ scripts/0-create-database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE databasestudy;
+
+
+-- sudo lsof -i :5432 - Check if PostgreSQL is running
+-- docker exec -it databasestudy bash - Enter the container
+-- psql - Connect to the database
+-- \l - List the databases
+-- \c databasestudy - Connect to the databasestudy database
+-- \dt - List the tables
+-- \q - Quit the database
+-- exit - Exit the container
